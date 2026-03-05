8eedac48a1696a0ac1597c93fb1370e38c13bed1
rtlsq2007 <yechanyang77@gmail.com>
2026-03-06 00:35:05 +0900

    Coding Study 03.06
    Coding Study 03.06

---
+++ Coding_Study/Array_07.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    bool n[30] = {};
+
+    for (int i = 0; i < 28; i++) {
+        int x;
+        cin >> x;
+        n[x] = true;
+    }
+
+    for (int i = 1; i <= 30; i++) {
+        if (n[i] == false) {
+            cout << i << "\n";
+        }
+    }
+
+	return 0;
+}
